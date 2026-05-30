522cb46c4d89d77446533c1a62b6e2a7370e62ff
Lars Schumann <LarsSchumann@mail.de>
2026-05-30 16:34:10 +0200

    move tests and update readme

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-## What?
-Automatically calculates the length of Arrays for you.
+## auto_array
+A declarative macro to automatically calculate the length of arrays.\
+This is useful e.g. for arrays whose length could be changed by conditional compilation.
 
-## How?
+## Example
 ```rust
 use auto_array::auto_array;
-
 fn main() {
     auto_array! {
         // Additional attributes, docs, and visibility are supported.
-        /// A const array.
+        /// An unused const array.
         #[allow(unused)]
         const A: [u8; _] = [3, 3, 3];
-        /// A static array with conditional compilation and pub(crate) visibility.
+        /// A static array whose length depends on conditional compilation.
         pub(crate) static B: [u8; _] = [1, #[cfg(unix)] 2];
     }
     assert_eq!(A, [3, 3, 3]);
---
+++ tests/tests.rs
@@ -0,0 +1,88 @@
+use auto_array::auto_array;
+
+macro_rules! assert_type_and_value {
+        ($($lhs:path: $ty:ty = $rhs:expr;)*) => {
+            $(let _: $ty = $lhs;
+            assert_eq!($lhs, $rhs);)*
+        };
+    }
+
+#[test]
+fn single_arrays() {
+    auto_array!(
+        pub const A: [i32; _] = [1, 2, 3];
+    );
+    auto_array!(
+        pub static B: [i8; _] = [4, 4, 4, 4];
+    );
+    assert_type_and_value!(
+        A: [i32; 3] = [1, 2, 3];
+        B: [i8; 4] = [4, 4, 4, 4];
+    );
+}
+
+#[test]
+fn multiple_arrays() {
+    auto_array!(
+        const A: [i8; _] = [1, 2, 3];
+        const B: [i16; _] = [];
+        const C: [i32; _] = [4, 4, 4, 4];
+        const D: [i64; _] = [6, 6, 6, 6, 6, 6];
+    );
+    assert_type_and_value!(
+        A: [i8; 3] = [1, 2, 3];
+        B: [i16; 0] = [];
+        C: [i32; 4] = [4, 4, 4, 4];
+        D: [i64; 6] = [6, 6, 6, 6, 6, 6];
+    );
+
+    auto_array!(
+        static E: [u32; _] = [1];
+        static F: [u64; _] = [2, 2];
+    );
+    assert_type_and_value!(
+        E: [u32; 1] = [1];
+        F: [u64; 2] = [2, 2];
+    );
+}
+
+#[test]
+fn mix_const_and_static() {
+    auto_array!(
+        const A: [u8; _] = [2, 2];
+        static B: [u8; _] = [3, 3, 3];
+    );
+    assert_type_and_value!(
+        A: [u8; 2] = [2, 2];
+        B: [u8; 3] = [3, 3, 3];
+    );
+}
+
+#[test]
+#[deny(dead_code, clippy::approx_constant)]
+fn attributes() {
+    auto_array!(
+        #[allow(dead_code)]
+        #[allow(clippy::approx_constant)]
+        pub(crate) const UNUSED: [f32; _] = [3.14];
+    );
+}
+
+#[test]
+fn empty_invocation() {
+    auto_array!();
+}
+
+#[test]
+fn visibility() {
+    mod foo {
+        use auto_array::auto_array;
+
+        auto_array!(
+            pub(super) const A: [i32; _] = [1];
+        );
+    }
+    assert_type_and_value!(
+        foo::A: [i32; 1] = [1];
+    );
+}
